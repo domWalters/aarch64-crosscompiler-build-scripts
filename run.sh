95360888a51d7de254327bdb8512bbaee77a898f
Dominic Walters <waltersdom@gmail.com>
2020-01-25 13:47:51 +0000

    Switched to indenting with 2 spaces rather than 4.

--- run.sh
+++ run.sh
@@ -20,8 +20,8 @@ LINUX_ARCH=arm64
 CONFIGURATION_OPTIONS="--disable-multilib" # --disable-threads --disable-shared
 PARALLEL_MAKE=-j4
 case ":${PATH:=$INSTALL_PATH/bin}:" in
-    *:"$INSTALL_PATH/bin":*)    ;;
-    *) export PATH=$INSTALL_PATH/bin:$PATH;;
+  *:"$INSTALL_PATH/bin":*)    ;;
+  *) export PATH=$INSTALL_PATH/bin:$PATH;;
 esac
 
 # Step 1. Binutils
@@ -34,16 +34,16 @@ cd ..
 
 # Step 2. Linux Kernel Headers
 if [ $USE_NEWLIB -eq 0 ]; then
-    cd $LINUX_KERNEL_VERSION
-    make ARCH=$LINUX_ARCH INSTALL_HDR_PATH=$INSTALL_PATH/$TARGET headers_install
-    cd ..
+  cd $LINUX_KERNEL_VERSION
+  make ARCH=$LINUX_ARCH INSTALL_HDR_PATH=$INSTALL_PATH/$TARGET headers_install
+  cd ..
 fi
 
 # Step 3. C/C++ Compilers
 mkdir -p build-gcc
 cd build-gcc
 if [ $USE_NEWLIB -ne 0 ]; then
-    NEWLIB_OPTION=--with-newlib
+  NEWLIB_OPTION=--with-newlib
 fi
 ../$GCC_VERSION/configure --prefix=$INSTALL_PATH --target=$TARGET --enable-languages=c,c++ $CONFIGURATION_OPTIONS $NEWLIB_OPTION
 make $PARALLEL_MAKE all-gcc
@@ -51,36 +51,36 @@ make install-gcc
 cd ..
 
 if [ $USE_NEWLIB -ne 0 ]; then
-    # Steps 4-6: Newlib
-    mkdir -p build-newlib
-    cd build-newlib
-    ../newlib-master/configure --prefix=$INSTALL_PATH --target=$TARGET $CONFIGURATION_OPTIONS
-    make $PARALLEL_MAKE
-    make install
-    cd ..
+  # Steps 4-6: Newlib
+  mkdir -p build-newlib
+  cd build-newlib
+  ../newlib-master/configure --prefix=$INSTALL_PATH --target=$TARGET $CONFIGURATION_OPTIONS
+  make $PARALLEL_MAKE
+  make install
+  cd ..
 else
-    # Step 4. Standard C Library Headers and Startup Files
-    mkdir -p build-glibc
-    cd build-glibc
-    ../$GLIBC_VERSION/configure --prefix=$INSTALL_PATH/$TARGET --build=$MACHTYPE --host=$TARGET --target=$TARGET --with-headers=$INSTALL_PATH/$TARGET/include $CONFIGURATION_OPTIONS libc_cv_forced_unwind=yes
-    make install-bootstrap-headers=yes install-headers
-    make $PARALLEL_MAKE csu/subdir_lib
-    install csu/crt1.o csu/crti.o csu/crtn.o $INSTALL_PATH/$TARGET/lib
-    $TARGET-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o $INSTALL_PATH/$TARGET/lib/libc.so
-    touch $INSTALL_PATH/$TARGET/include/gnu/stubs.h
-    cd ..
+  # Step 4. Standard C Library Headers and Startup Files
+  mkdir -p build-glibc
+  cd build-glibc
+  ../$GLIBC_VERSION/configure --prefix=$INSTALL_PATH/$TARGET --build=$MACHTYPE --host=$TARGET --target=$TARGET --with-headers=$INSTALL_PATH/$TARGET/include $CONFIGURATION_OPTIONS libc_cv_forced_unwind=yes
+  make install-bootstrap-headers=yes install-headers
+  make $PARALLEL_MAKE csu/subdir_lib
+  install csu/crt1.o csu/crti.o csu/crtn.o $INSTALL_PATH/$TARGET/lib
+  $TARGET-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o $INSTALL_PATH/$TARGET/lib/libc.so
+  touch $INSTALL_PATH/$TARGET/include/gnu/stubs.h
+  cd ..
 
-    # Step 5. Compiler Support Library
-    cd build-gcc
-    make $PARALLEL_MAKE all-target-libgcc
-    make install-target-libgcc
-    cd ..
+  # Step 5. Compiler Support Library
+  cd build-gcc
+  make $PARALLEL_MAKE all-target-libgcc
+  make install-target-libgcc
+  cd ..
 
-    # Step 6. Standard C Library & the rest of Glibc
-    cd build-glibc
-    make $PARALLEL_MAKE
-    make install
-    cd ..
+  # Step 6. Standard C Library & the rest of Glibc
+  cd build-glibc
+  make $PARALLEL_MAKE
+  make install
+  cd ..
 fi
 
 # Step 7. Standard C++ Library & the rest of GCC
--- setup.sh
+++ setup.sh
@@ -4,62 +4,62 @@ cd "${0%/*}"
 source ./versions.sh
 
 if [ "$#" -gt 1 ]; then
-    echo "Too many arguments."
-    echo "Usage: ./setup.sh [--download --unpack]"
+  echo "Too many arguments."
+  echo "Usage: ./setup.sh [--download --unpack]"
 fi
 
 while true; do
-    read -p "Warning: This script is going to delete $INSTALL_PATH. Are you sure you wish to continue?" yn
-    case $yn in
-        [Yy]* ) 
-        rm -r $INSTALL_PATH || true 
-        rm -r build-*/ || true
-        break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer, yes or no.";;
-    esac
+  read -p "Warning: This script is going to delete $INSTALL_PATH. Are you sure you wish to continue? (y/n))" yn
+  case $yn in
+    [Yy]* )
+    rm -r $INSTALL_PATH || true
+    rm -r build-*/ || true
+    break;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer, y or n.";;
+  esac
 done
 
 case $1 in
-    "--download" ) 
-        # Download packages
-        rm *.tar.*
-        wget -nc https://ftp.gnu.org/gnu/binutils/$BINUTILS_VERSION.tar.gz
-        wget -nc https://ftp.gnu.org/gnu/gcc/$GCC_VERSION/$GCC_VERSION.tar.gz
-        if [ $USE_NEWLIB -ne 0 ]; then
-            wget -nc -O newlib-master.zip https://github.com/bminor/newlib/archive/master.zip || true
-            unzip -qo newlib-master.zip
-        else
-            wget -nc https://www.kernel.org/pub/linux/kernel/v4.x/$LINUX_KERNEL_VERSION.tar.xz
-            wget -nc https://ftp.gnu.org/gnu/glibc/$GLIBC_VERSION.tar.xz
-        fi
-        wget -nc https://ftp.gnu.org/gnu/mpfr/$MPFR_VERSION.tar.xz
-        wget -nc https://ftp.gnu.org/gnu/gmp/$GMP_VERSION.tar.xz
-        wget -nc https://ftp.gnu.org/gnu/mpc/$MPC_VERSION.tar.gz
-        wget -nc ftp://gcc.gnu.org/pub/gcc/infrastructure/$ISL_VERSION.tar.bz2
-        wget -nc ftp://gcc.gnu.org/pub/gcc/infrastructure/$CLOOG_VERSION.tar.gz;&
-    "--unpack" ) 
-        # Remove unpack locations
-        rm -r binutils-*/ || true
-        rm -r cloog-*/ || true
-        rm -r gcc-*/ || true
-        rm -r glibc-*/ || true
-        rm -r gmp-*/ || true
-        rm -r isl-*/ || true
-        rm -r linux-*/ || true
-        rm -r mpc-*/ || true
-        rm -r mpfr-*/ || true
-        # Unpack
-        for f in *.tar*; do 
-            tar xfk $f
-        done
-        # Make symbolic links
-        cd $GCC_VERSION
-        ln -sf `ls -1d ../mpfr-*/` mpfr
-        ln -sf `ls -1d ../gmp-*/` gmp
-        ln -sf `ls -1d ../mpc-*/` mpc
-        ln -sf `ls -1d ../isl-*/` isl
-        ln -sf `ls -1d ../cloog-*/` cloog
-        cd ..;;
-    * ) echo "Invalid argument: $var" && exit;;
+  "--download" )
+    # Download packages
+    rm *.tar.*
+    wget -nc https://ftp.gnu.org/gnu/binutils/$BINUTILS_VERSION.tar.gz
+    wget -nc https://ftp.gnu.org/gnu/gcc/$GCC_VERSION/$GCC_VERSION.tar.gz
+    if [ $USE_NEWLIB -ne 0 ]; then
+      wget -nc -O newlib-master.zip https://github.com/bminor/newlib/archive/master.zip || true
+      unzip -qo newlib-master.zip
+    else
+      wget -nc https://www.kernel.org/pub/linux/kernel/v4.x/$LINUX_KERNEL_VERSION.tar.xz
+      wget -nc https://ftp.gnu.org/gnu/glibc/$GLIBC_VERSION.tar.xz
+    fi
+    wget -nc https://ftp.gnu.org/gnu/mpfr/$MPFR_VERSION.tar.xz
+    wget -nc https://ftp.gnu.org/gnu/gmp/$GMP_VERSION.tar.xz
+    wget -nc https://ftp.gnu.org/gnu/mpc/$MPC_VERSION.tar.gz
+    wget -nc ftp://gcc.gnu.org/pub/gcc/infrastructure/$ISL_VERSION.tar.bz2
+    wget -nc ftp://gcc.gnu.org/pub/gcc/infrastructure/$CLOOG_VERSION.tar.gz;&
+  "--unpack" )
+    # Remove unpack locations
+    rm -r binutils-*/ || true
+    rm -r cloog-*/ || true
+    rm -r gcc-*/ || true
+    rm -r glibc-*/ || true
+    rm -r gmp-*/ || true
+    rm -r isl-*/ || true
+    rm -r linux-*/ || true
+    rm -r mpc-*/ || true
+    rm -r mpfr-*/ || true
+    # Unpack
+    for f in *.tar*; do
+      tar xfk $f
+    done
+    # Make symbolic links
+    cd $GCC_VERSION
+    ln -sf `ls -1d ../mpfr-*/` mpfr
+    ln -sf `ls -1d ../gmp-*/` gmp
+    ln -sf `ls -1d ../mpc-*/` mpc
+    ln -sf `ls -1d ../isl-*/` isl
+    ln -sf `ls -1d ../cloog-*/` cloog
+    cd ..;;
+  * ) echo "Invalid argument: $var" && exit;;
 esac
